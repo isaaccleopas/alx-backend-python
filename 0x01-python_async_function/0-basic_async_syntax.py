57b09392446bc5ba939998d7b3864acb798faeee
isaaccleopas <isaaccleopas@gmail.com>
2023-08-07 11:22:00 +0000

    task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Asynchronous coroutine that takes in an integer argument"""
 
-
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Returns a random float between 0 and 10"""
