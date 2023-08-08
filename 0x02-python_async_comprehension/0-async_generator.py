436e2d2178735bee35ee1128b7ddc56128431bd5
isaaccleopas <isaaccleopas@gmail.com>
2023-08-08 12:10:42 +0000

    task 0 correction

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """This coroutine will loop 10x, each time asynchronously wait 1 second"""
-
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[int, None, None]:
     """Sets the the range and await i sec"""
     for i in range(10):
         await asyncio.sleep(1)
