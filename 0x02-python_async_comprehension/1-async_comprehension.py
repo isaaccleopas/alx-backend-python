3686ae49b39598b25a89f3526c4433f7a07fe6df
isaaccleopas <isaaccleopas@gmail.com>
2023-08-08 13:05:59 +0000

    task 1 corrected

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """This coroutine will collect 10 random numbers from async_generator."""
 from typing import List
-
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
