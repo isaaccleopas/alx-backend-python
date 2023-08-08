b7753722f0bfad539d3bdc6478192113f98ed7b7
isaaccleopas <isaaccleopas@gmail.com>
2023-08-08 12:20:41 +0000

    correction of task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,8 +4,9 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[int, None, None]:
-    """Sets the the range and await i sec"""
+    """Returns a random num in range 10 and await i sec"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
