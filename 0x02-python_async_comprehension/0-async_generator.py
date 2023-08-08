a765a2f57f290ed0d7d0f617d111f50c7402ce99
isaaccleopas <isaaccleopas@gmail.com>
2023-08-08 12:24:00 +0000

    task 0 generator type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Returns a random num in range 10 and await i sec"""
     for i in range(10):
         await asyncio.sleep(1)
